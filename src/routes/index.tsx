37ef79e4e7802adbfa602379e31b39af78d57967
PixelPopLk <pasindumadusanka3339@gmail.com>
2026-09-12 02:39:04 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
+import { DmcaModal } from "@/components/DmcaModal";
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { motion, AnimatePresence } from "framer-motion";
 import {
